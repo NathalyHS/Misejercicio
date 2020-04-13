ea90417c0f2e92ec326018c98557039dca93efbc
Nathalia H.S <liat7002@gmail.com>
2020-04-13 18:03:12 -0500

    first commit

---
+++ atributos/atributos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+
+    {
+        aprendiz apsena = new aprendiz();
+        apsena.Nombres = "Nathalia";
+        apsena.apellidos = "Hernandez Santos";
+        apsena.Direccion = "cll A 23 # 45 ";
+        saludo(apsena); 
+    }
+    private static void saludo(aprendiz apsena)
+    {
+        Console.WriteLine("bienvenido, sr(a )" + apsena.Nombres);
+        Console.WriteLine(apsena.apellidos);
+         Console.WriteLine(apsena.Direccion);
+        Console.ReadLine();
+    }
+}
+
